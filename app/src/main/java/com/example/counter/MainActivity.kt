544e7ef199ac4d5cf48bb84dec7dc051cf352843
Justyn Mencl <men19033@byui.edu>
2022-05-30 17:05:20 -0600

    initial commit

---
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var countByInput : EditText
+    private lateinit var countOutput : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val count_button = findViewById<Button>(R.id.count)
+        val reset_button = findViewById<Button>(R.id.reset)
+
+        var count = 0
+        count_button.setOnClickListener {
+            count += countByInput.text.toString().toInt()
+            countOutput.text = count.toString()
+        }
+        reset_button.setOnClickListener { reset_count() }
+
+
+        countByInput = findViewById<EditText>(R.id.CountBy_input)
+        countOutput = findViewById<TextView>(R.id.count_output)
+    }
+
+    private fun reset_count() {
+
+    }
+}
+
